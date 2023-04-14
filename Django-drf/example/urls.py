04492af3c3b079e01c25570756c014272b4ebe40
daeun han <daeunhan0723@gmail.com>
2023-04-14 16:30:54 +0900

    Chapter 4

--- Django-drf/example/urls.py
+++ Django-drf/example/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import HelloAPI, bookAPI, booksAPI, BooksAPI, BookAPI
+from .views import HelloAPI, bookAPI, booksAPI, BooksAPI, BookAPI, BooksAPIMixins, BookAPIMixins, BooksAPIGenerics, BookAPIGenerics
 
 urlpatterns = [
     path("hello/", HelloAPI),
@@ -7,4 +7,18 @@ urlpatterns = [
     path("fbv/book/<int:bid>/", bookAPI),
     path("cbv/books/", BooksAPI.as_view()),
     path("cbv/book/<int:bid>/", BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
+    path("generic/books/", BooksAPIGenerics.as_view()),
+    path("generic/book/<int:bid>/", BookAPIGenerics.as_view()),
 ]
+
+from rest_framework import routers
+from .views import BookViewSet
+
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+
+urlpatterns = router.urls
+
+

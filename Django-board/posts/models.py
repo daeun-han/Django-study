f163be0a9614059a5b8daca9929616509a8b5dc2
daeun han <daeunhan0723@gmail.com>
2023-05-21 02:39:36 +0900

    Chapter 6
    배포 제외

--- Django-board/posts/models.py
+++ Django-board/posts/models.py
@@ -6,7 +6,7 @@ from users.models import Profile
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
     title = models.CharField(max_length=128)
     category = models.CharField(max_length=128)
@@ -14,3 +14,9 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post/', default='default.png')
     likes = models.ManyToManyField(User, related_name='like_posts', blank=True)
     published_date = models.DateTimeField(default=timezone.now)
+    
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()

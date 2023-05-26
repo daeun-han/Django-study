774a63f1430812adef574b1a0fc0bb604d8a49f3
daeun han <daeunhan0723@gmail.com>
2023-05-26 10:32:21 +0900

    게시글 모델 수정 전

--- Django-myboard/posts/models.py
+++ Django-myboard/posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from users.models import Profile
+
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
+    title = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
+    body = models.TextField()
+    image = models.ImageField(upload_to='post/', default='default.png')
+    likes = models.ManyToManyField(User, related_name='like_posts', blank=True)
+    # like_count = models.PositiveIntegerField(default=0) # 0또는 양수만 받는 필드
+    lion_emoji = models.ManyToManyField(User, related_name='lion_emoji_posts', blank=True)
+    # lion_emoji_count = models.PositiveIntegerField(default=0) # 0또는 양수만 받는 필드
+    published_date = models.DateTimeField(default=timezone.now)
